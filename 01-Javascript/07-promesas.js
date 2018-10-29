a1c80e1e64e47cccaac8293271a0d35e09029078
David Mejia <franklin.mejia@epn.edu.ec>
2018-10-29 11:03:33 -0500

    AppendFile Function

--- 01-Javascript/07-promesas.js
+++ 01-Javascript/07-promesas.js
@@ -53,3 +53,64 @@ nuevaPromesaLectura
             console.log('Algo malo paso', resultadoError);
         }
     );
+
+const nuevaPromesaAppendFile = (nombreArchivo,contenidoArchivo) => {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(nombreArchivo,'utf-8',
+                (error,contenidoArchivoLeido)=>{
+                    if (error){
+                        fs.writeFile(nombreArchivo,contenidoArchivo,
+                            (err)=>{
+                                if (err) {
+                                    reject(console.error('Error escribiendo'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                                                            }
+                        );
+
+                    }else{
+                        fs.writeFile(nombreArchivo,contenidoArchivoLeido+contenidoArchivo,
+                            (err)=>{
+                                if (err) {
+                                    reject(console.error('Error escribiendo'));
+                                } else {
+                                    resolve(contenidoArchivo);
+                                }
+                            }
+                        );
+                    }
+
+                }
+
+
+            );
+
+        }
+    );
+};
+
+nuevaPromesaAppendFile()
+    .then(
+        (resultadoOk) => {
+            console.log('Todo bien', resultadoOk);
+            return nuevaPromesaAppendFile(contenidoArchivo,contenidoArchivo)
+
+        }
+    )
+       .catch(
+        (resultadoError) => {
+            console.log('Algo malo paso', resultadoError);
+        }
+    );
+
+nuevaPromesaAppendFile('06-texto3.text','\n Adios Mundo',
+    (contenidoArchivo,error)=>{
+        if(error){
+            console.log('Error',error)
+        }else{
+            console.log(contenidoArchivo)
+        }
+    }
+);
